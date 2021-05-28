594ea18a8f132460255c8d733274cd4080b38372
DARLANALONE <80855549+DARLANALONE@users.noreply.github.com>
2021-05-27 23:11:31 -0500

    actualizacion
    calendar

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('Calendar/event','ControllerCalendar@index');
+    Route::get('Calendar/event/{mes}','ControllerCalendar@index_month');
